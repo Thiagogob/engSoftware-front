d9761f9d56413ab8b0964e7c8c4327e7ae6e288c
Thiago <thiagogobb@outlook.com>
2023-04-18 09:41:36 -0300

    chore lp

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,42 +2,36 @@ import "./style.css";
 
 const Home = () => (
   <div className="container">
-    <nav className="navbar navbar-expand-lg navbar-light bg-nav">
-      <a className="navbar-brand" href="#">
-        <div className="text-nav">
-          Fauna<b>Snapshot</b>
-        </div>
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div
-        className="collapse navbar-collapse d-flex justify-content-end"
-        id="navbarNav"
-      >
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <div className="text-nav">Instruções</div>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              <div className="text-nav">Documentação</div>
-            </a>
-          </li>
-        </ul>
-      </div>
+    <div className="row">
+
+    <nav className="navbar navbar-light bg-light">
+      <span className="navbar-brand mb-0 h1">Fauna<b>Snapshot</b></span>
     </nav>
+
+    </div>
+
+    <img src="jungleBackground.jpg" alt="" />
+
+    <div class="jumbotron">
+      <h1 class="display-4">Hello, world!</h1>
+      <p class="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste officiis exercitationem in atque odit iusto ducimus minus quos eligendi veritatis expedita distinctio iure alias autem reprehenderit, aspernatur harum sequi quae!</p>
+      <hr class="my-4"></hr>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ipsum quos voluptates! Odio natus quibusdam labore tenetur neque vero, pariatur totam impedit aspernatur? Beatae, optio molestias. Itaque beatae ad sunt.</p>
+    </div>
+
+    <div className="col">
+      <div className="row my-5">
+        <button type="button" className="btn btn-primary btn-lg btn-block">Iniciar</button>
+      </div>
+
+      <div classNameName="row">
+        <button type="button" className="btn btn-primary btn-lg btn-block">Como Jogar</button>
+      </div>
+    </div>
+
+
   </div>
+
 );
 
 export default Home;
