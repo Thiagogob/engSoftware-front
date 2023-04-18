e57c96f5e47360ea5e6114a116f0f578d5723fe0
Thiago <thiagogobb@outlook.com>
2023-04-18 15:06:53 -0300

    chore new main menu

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,36 +1,15 @@
 import "./style.css";
-
 const Home = () => (
-  <div className="container">
-    <div className="row">
-
-    <nav className="navbar navbar-light bg-light">
-      <span className="navbar-brand mb-0 h1">Fauna<b>Snapshot</b></span>
-    </nav>
-
-    </div>
-
-    <img src="jungleBackground.jpg" alt="" />
-
-    <div class="jumbotron">
-      <h1 class="display-4">Hello, world!</h1>
-      <p class="lead">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste officiis exercitationem in atque odit iusto ducimus minus quos eligendi veritatis expedita distinctio iure alias autem reprehenderit, aspernatur harum sequi quae!</p>
-      <hr class="my-4"></hr>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis ipsum quos voluptates! Odio natus quibusdam labore tenetur neque vero, pariatur totam impedit aspernatur? Beatae, optio molestias. Itaque beatae ad sunt.</p>
-    </div>
-
-    <div className="col">
-      <div className="row my-5">
-        <button type="button" className="btn btn-primary btn-lg btn-block">Iniciar</button>
-      </div>
-
-      <div classNameName="row">
-        <button type="button" className="btn btn-primary btn-lg btn-block">Como Jogar</button>
+  <body>
+    <div className="container">
+      <div class="menu-box jumbotron">
+        <h1 class="display-4 project-text my-4">Fauna Snapshot</h1>
+        <button type="button" class="btn btn-outline-secondary btn-play my-2">Jogar</button>
+        <p class="lead how-to-play-secondary"><img src="/src/assets/mouse.png" alt="" /><b className="how-to-play-main">  Como jogar: </b>Aponte o mouse para os animais<br /> 
+        e clique com o botão direito do mouse</p>
       </div>
     </div>
-
-
-  </div>
+  </body>
 
 );
 
