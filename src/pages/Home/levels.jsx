a2c899da92825ee7e18f4578c01e7ce0878dc939
Thiago <thiagogobb@outlook.com>
2023-04-19 09:45:21 -0300

    chore levels page

---
+++ src/pages/Home/levels.jsx
@@ -0,0 +1,25 @@
+import "./style.css";
+const Levels = () => (
+    <body>
+        <div className="container">
+            <div className="card-deck my-5">
+                    <img src="..." class="card-img-top" alt="..."></img>
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                        <img src="..." class="card-img-top" alt="..."></img>
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    
+            </div>
+        </div>
+    </body>
+
+);
+
+export default Levels;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import { BrowserRouter } from "react-router-dom";
+import Levels from "./pages/Home/levels";
 
 const AppRoutes = () => (
   <BrowserRouter>
     <Routes>
       <Route path="/">
         <Route index element={<Home />} />
+      </Route>
+      <Route path="/levels">
+        <Route index element={<Levels />} />
       </Route>
     </Routes>
   </BrowserRouter>
