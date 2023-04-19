c6e5c701bbc4d5c1137ded632e5919a36fdd941c
Thiago <thiagogobb@outlook.com>
2023-04-19 19:19:57 -0300

    fix images verce 1.2

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,7 +5,7 @@ const Home = () => (
       <div class="menu-box jumbotron">
         <h1 class="display-4 project-text my-4">Fauna Snapshot</h1>
         <a href="/levels"><button type="button" class="btn btn-outline-secondary btn-play my-2">Jogar</button></a>
-        <p class="lead how-to-play-secondary"><img src="/src/static/mouse.png" alt="" /><b className="how-to-play-main">  Como jogar: </b>Aponte o mouse para os animais<br /> 
+        <p class="lead how-to-play-secondary"><img src={"/src/static/mouse.png"} alt="" /><b className="how-to-play-main">  Como jogar: </b>Aponte o mouse para os animais<br /> 
         e clique com o botão direito do mouse</p>
       </div>
     </div>
