4ae370cf2173e714e60711ecfeb9dfab882c3032
Thiago <thiagogobb@outlook.com>
2023-04-19 20:51:36 -0300

    chore selecao fase

--- src/pages/Home/levels.jsx
+++ src/pages/Home/levels.jsx
@@ -2,19 +2,25 @@ import "./style.css";
 const Levels = () => (
 
     <div className="container">
-        <div className="row">
-            <div className="col-6 card-border">
-                <img src={"/public/static/images/level1.jpg"} class="card-img-top" alt="..."></img>
-                <div class="card-body card-bg">
-                    <h5 class="card-title d-flex justify-content-center">Fase 1</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <div className="row d-flex justify-content-center">
+            <h1 className="my-3 project-text">Seleção de Fase: <span className="badge badge-secondary"></span></h1>
+        </div>
+        <div className="row d-flex justify-content-center">
+            <div className="col-4 card-border">
+                <img src={"/static/images/level1.png"} className="card-img-top img-level" alt="..."></img>
+                <div className="card-body card-bg">
+                    <h5 className="card-title d-flex justify-content-center">Fase 1</h5>
+                    <p className="card-text justify-card-text">Nessa fase, o jogador é apresentado a diferentes espécies de animais da fauna brasileira e é desafiado a identificá-los pelo nome e pelo som que eles emitem.</p>
+                    <div className="d-flex justify-content-center"><button type="button" className="btn btn-primary btn-decoration">Jogar Fase 1</button></div>
                 </div>
             </div>
-            <div className="col-6 card-border">
-                <img src={"/public/static/images/level2.jpg"} class="card-img-top" alt="..."></img>
-                <div class="card-body card-bg">
-                    <h5 class="card-title d-flex justify-content-center">Fase 2</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <div className="col-1"></div>
+            <div className="col-4 card-border">
+                <img src={"/static/images/level2.png"} className="card-img-top img-level" alt="..."></img>
+                <div className="card-body card-bg">
+                    <h5 className="card-title d-flex justify-content-center">Fase 2</h5>
+                    <p className="card-text justify-card-text">Nessa fase, o jogador é desafiado a encontrar e fotografar diferentes espécies de animais que habitam as florestas, pantanais, e outros ecossistemas do Brasil.</p>
+                    <div className="d-flex justify-content-center"><button type="button" className="btn btn-primary btn-decoration">Jogar Fase 2</button></div>
                 </div>
             </div>
         </div>
