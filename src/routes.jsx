a033b676069388e9529b7708df81793732b57e3a
Thiago <thiagogobb@outlook.com>
2023-04-20 08:32:49 -0300

    motion framework

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router";
 import Home from "./pages/Home";
 import { BrowserRouter } from "react-router-dom";
 import Levels from "./pages/Home/levels";
-import Level1 from "./pages/Home/level2";
+import Level1 from "./pages/Home/level1";
 
 const AppRoutes = () => (
   <BrowserRouter>
