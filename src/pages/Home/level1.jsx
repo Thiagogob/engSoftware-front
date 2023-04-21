dfc9822a697507b6c2d6c69731db08d297007f90
Thiago <thiagogobb@outlook.com>
2023-04-21 11:38:57 -0300

    chore multiple question setup

--- src/pages/Home/level1.jsx
+++ src/pages/Home/level1.jsx
@@ -1,23 +1,109 @@
-import "./style.css"
+import React, { useState } from "react";
+import "./style.css";
+import oncaPintada from "/static/images/oncapintada.png";
+import arara from "/static/images/arara.png";
+import capivara from "/static/images/capivara.png";
+import cobra from "/static/images/cobra.png";
+import crocodilo from "/static/images/crocodilo.png";
+import flamingo from "/static/images/flamingo.png";
+import juburu from "/static/images/juburu.png";
+import macaco from "/static/images/macaco.png";
+
 function Level1() {
-    
-    return (
+  //propriedades
+  const [showFinalResults, setFinalResults] = useState(false);
+  const [score, setScore] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(3);
+
+  const questions = [
+    {
+      text: "Qual o nome desse animal:",
+      image: <img src={oncaPintada}/>,
+      options: [
+        { id: 0, text: "Onça-pintada", isCorrect: false },
+        { id: 1, text: "Flamingo", isCorrect: false },
+        { id: 2, text: "Arara", isCorrect: false },
+      ],
+    },
+    {
+      text: "Qual o som desse animal: ",
+      image: <img src={oncaPintada}/>,
+      options: [
+        { id: 0, text: "1787", isCorrect: true },
+        { id: 1, text: "1776", isCorrect: false },
+        { id: 2, text: "1774", isCorrect: false },
+      ],
+    },
+    {
+      text: "Qual o nome desse animal:",
+      image: <img src={macaco}/>,
+      options: [
+        { id: 0, text: "Macaco", isCorrect: true },
+        { id: 1, text: "Cobra", isCorrect: false },
+        { id: 2, text: "Juburu", isCorrect: false },
+      ],
+    },
+    {
+      text: "Qual o som desse animal: ",
+      image: <img src={macaco}/>,
+      options: [
+        { id: 0, text: "California", isCorrect: false },
+        { id: 1, text: "Alaska", isCorrect: true },
+        { id: 2, text: "Texas", isCorrect: false },
+      ],
+    },
+    {
+      text: "Qual o nome desse animal:",
+      image: <img src={flamingo}/>,
+      options: [
+        { id: 0, text: "Flamingo", isCorrect: false },
+        { id: 1, text: "Arara", isCorrect: true },
+        { id: 2, text: "Crocodilo", isCorrect: true },
+      ],
+    },
+    {
+        text: "Qual o som desse animal:",
+        image: <img src={flamingo}/>,
+        options: [
+          { id: 0, text: "Flamingo", isCorrect: false },
+          { id: 1, text: "Arara", isCorrect: true },
+          { id: 2, text: "Crocodilo", isCorrect: true },
+        ],
+      },
+  ];
+
+
+  return (
     <div className="container">
-        <div className="jumbotron jumbotron-fluid animal-jumbotron">
-            <div className="container">
-                <div className="animal-img d-flex justify-content-center"><img src={"/static/images/oncapintada.png"} alt="" /></div>
-                <div className="d-flex justify-content-center">
-                    <p className="lead project-text">Qual o nome desse animal: </p>
-                </div>
-                <div className="row d-flex justify-content-center">
-                    <button type="button" className="btn btn-primary btn-decoration mx-auto btn-animal btn-decoration">Onça-pintada</button>
-                    <button type="button" className="btn btn-primary btn-decoration mx-auto btn-animal btn-decoration">Flamingo</button>
-                    <button type="button" className="btn btn-primary btn-decoration mx-auto btn-animal btn-decoration">Arara</button>
-                </div>
+      <div className="jumbotron jumbotron-fluid animal-jumbotron">
+      <h2 className="score-text">Quantidade de Pontos: {score}</h2>
+        {showFinalResults ? (
+          <div className="final-result d-flex justify-content-center">
+            <button>Sair</button>
+            <button>Fase 2</button>
+          </div>
+        ) : (
+          <div className="question-card">
+            <div className="animal-img d-flex justify-content-center">
+                {questions[currentQuestion].image}
             </div>
-        </div>
+            <p className="lead project-text d-flex justify-content-center question-text">
+              {questions[currentQuestion].text}
+            </p>
+
+            <ul className="d-flex justify-content-center list-style">
+              {questions[currentQuestion].options.map((option) =>{
+                return (
+                    <li type="button"
+                    className="btn btn-primary btn-decoration mx-auto btn-animal btn-decoration" key={option.id}>{option.text}</li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
-);
+  );
 }
 
 export default Level1;
