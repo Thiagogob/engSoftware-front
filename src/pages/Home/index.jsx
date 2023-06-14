776f0dedc09fe9a20aee269e86529ad2b72bb152
Thiago <thiagogobb@outlook.com>
2023-06-14 17:29:37 -0300

    chore text transform uppercase

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,9 +4,6 @@ const Home = () => (
       <div className="menu-box jumbotron">
         <h1 className="display-4 project-text my-4">Fauna Snapshot</h1>
         <a href="/levels"><button type="button" className="btn btn-outline-secondary btn-play my-2">Jogar</button></a>
-        <p className="lead how-to-play-secondary"><img src={"/static/images/mouse.png"} alt="" /><b className="how-to-play-main">  Como jogar: </b>Clique no botão "Jogar" para começar.<br />
-        Mais instruções aparecerão no decorrer do jogo. 
-        </p>
       </div>
     </div>
 
