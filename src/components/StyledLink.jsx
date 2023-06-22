2d83c97bcbc55c9f45ab77b62af626c11fa61eef
Gabriel Stawny <gastawny@gmail.com>
2023-06-21 21:14:31 -0300

    Change Home page

--- src/components/StyledLink.jsx
+++ src/components/StyledLink.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 const StyledLink = styled(Link)`
   background-color: #111111;
   color: #FBFBFB;
-  padding: .75rem 1.5rem;
+  padding: 1rem 1.75rem;
   letter-spacing: .25rem;
-  border-radius: 5px;
+  border-radius: 10px;
   font-weight: 500;
-  font-size: 1.5rem;
+  font-size: 1.75rem;
   transition: ease-in-out 200ms;
 
   &:hover {
--- src/styles/globals.js
+++ src/styles/globals.js
@@ -10,8 +10,4 @@ export default createGlobalStyle`
     font-weight: 700;
     text-transform: uppercase;
   }
-
-  a {
-    color: red;
-  }
 `;
